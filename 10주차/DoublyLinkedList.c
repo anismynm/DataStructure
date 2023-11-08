b7efc5f91f589bd5e9b65e3831a48d79918df7c4
anismynm <ddoe0318@naver.com>
2023-11-08 12:58:42 +0900

    jj

--- 10주차/DoublyLinkedList.c
+++ 10주차/DoublyLinkedList.c
@@ -1,79 +1,175 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct list_node *list_pointer;
 typedef struct list_node {
-    int data;
+    char name[50];
+    double score;
     list_pointer llink; 
     list_pointer rlink;
 } list_node;
 
-list_node* head;
+list_pointer head = NULL;
 
-list_node* CreateNode() { // 노드만 생성 (노드 속 데이터는 지정 X)
-    list_node* new;
-    new = (list_node*)malloc(sizeof(list_node));
-    new -> data = 10;
-    new -> link = NULL;
-    return new;
-}
-
-list_node* CreateTwoNodes() {
-    list_node* new1;
-    list_node* new2;
-    new1 = (list_node*)malloc(sizeof(list_node));
-    new2 = (list_node*)malloc(sizeof(list_node));
-
-    new1 -> data = 0;
-    new1 -> link = new2;
-    new2 -> data = 0;
-    new2 -> link = NULL;
-
-    return new1;
-}
-
-void Insert(list_node* p, list_node* new, int data) { // p node 뒤에 new node 삽입
-    if(p) {
-        new -> link = p -> link;
-        p -> link = new;
-        new -> data = data;
+void Insert() {
+    list_pointer newStudent = (list_pointer)malloc(sizeof(list_node));
+    if (newStudent == NULL) {
+        printf("메모리 할당 오류!\n");
+        return;
     }
-    else { // 리스트의 가장 앞에 삽입
-        new -> link = head;
-        head = new;
-    }
-}
+    
+    printf("학생의 이름을 입력하시오. >> ");
+    rewind(stdin); // 입력 버퍼 제거
+    scanf("%s", newStudent -> name);
+    printf("학생의 성적을 입력하시오. >> ");
+    rewind(stdin);
+    scanf("%lf", &newStudent -> score);
 
-void Delete(list_node* p, list_node* node) {
-    if (p) {
-        p -> link = node -> link;
-    }
-    else {
-        head = head -> link;
-    }
-    free(node);
-}
+    newStudent -> llink = NULL;
+    newStudent -> rlink = NULL;
 
-list_node* Search(list_node* head, int data) {
-    while(head) {
-        if (head -> data == data) {
-            return head;
+    if (head == NULL) {
+        head = newStudent;
+        printf("학생의 정보가 추가되었습니다.\n");
+        return;
+    }
+    else { // Linked List에 점수 높은 순으로 삽입
+        // 입력 받은 점수가 1등일 때
+        if (newStudent -> score >= head -> score) {
+            head -> llink = newStudent;
+            newStudent -> rlink = head;
+            head = newStudent;
+            printf("학생의 정보가 추가되었습니다.\n");
+            return;
         }
-        head = head -> link;
+        // 1등이 아닌경우
+        list_pointer p = head;
+        while (p != NULL) {
+            if (p -> rlink == NULL) { // 꼴찌일 때
+                p -> rlink = newStudent;
+                newStudent -> llink = p;
+                printf("학생의 정보가 추가되었습니다.\n");
+                return;
+            }
+            else { // 꼴찌가 아닐 때
+                if (newStudent -> score >= p -> rlink -> score) {
+                    newStudent -> rlink = p -> rlink;
+                    newStudent -> llink = p;
+                    p -> rlink -> llink = newStudent;
+                    p -> rlink = newStudent;
+                    printf("학생의 정보가 추가되었습니다.\n");
+                    return;
+                }
+                else {
+                    p = p -> rlink;
+                }
+            }
+        }
     }
-    return NULL;
 }
 
-void Print(list_node* head) {
-    list_node* p = head;
-    while(p != NULL) {
-        printf("%d ", p -> data);
-        p = p -> link;
+void Delete() {
+    if (head == NULL) {
+        printf("학생의 정보가 존재하지 않습니다!\n");
+        return;
     }
-    printf("\n");
+
+    char NameForSearch[50];
+    printf("삭제할 학생의 이름을 입력하시오. >> ");
+    rewind(stdin);
+    scanf("%s", NameForSearch);
+
+    list_pointer p = head;
+    while (p != NULL) {
+        if (strcmp(p -> name, NameForSearch) == 0) {
+            if (p -> llink == NULL) {
+                head = p -> rlink;
+                if (head != NULL) {
+                    head -> llink = NULL;
+                }
+            }
+            else {
+                p -> llink -> rlink = p -> rlink;
+                if (p -> rlink != NULL) {
+                    p -> rlink -> llink = p -> llink;
+                }
+            }
+            free(p);
+            printf("학생의 정보가 삭제되었습니다.\n");
+            return;
+        }
+        else {
+            p = p -> rlink;
+        }
+    }
+    printf("검색 결과, 해당하는 학생의 정보가 존재하지 않습니다.\n");
+    return;
+}
+
+void Search() {
+    if (head == NULL) {
+        printf("학생의 정보가 존재하지 않습니다!\n");
+        return;
+    }
+
+    char NameForSearch[50];
+    printf("검색할 학생의 이름을 입력하시오. >> ");
+    rewind(stdin);
+    scanf("%s", NameForSearch);
+
+    list_pointer p = head;
+    int rank = 1;
+    while (p != NULL) {
+        if (strcmp(p -> name, NameForSearch) == 0) {
+            printf("%s 학생의 점수는 %.1f점이고 %d등입니다.\n", p -> name, p -> score, rank);
+            return;
+        }
+        else {
+            p = p -> rlink;
+            rank++;
+        }
+    }
+    printf("검색 결과, 해당하는 학생의 정보가 존재하지 않습니다.\n");
+    return;
 }
 
 int main() {
-    printf("=====Doubly Linked List=====\n");
-    
+    int iSelect;
+    do {
+        printf("┌──────────────────────────────────┐\n");
+        printf("│ 자료구조 시험 성적 관리 프로그램 │\n");
+        printf("│ 1. 학생 성적 입력                │\n");
+        printf("│ 2. 학생 정보 제거                │\n");
+        printf("│ 3. 학생 성적 검색                │\n");
+        printf("│ 4. 종료                          │\n");
+        printf("└──────────────────────────────────┘\n");
+        printf("메뉴 선택 >> ");
+        rewind(stdin);
+        scanf("%d", &iSelect);
+        switch(iSelect) {
+            case 1:
+            Insert();
+            break;
+            case 2:
+            Delete();
+            break;
+            case 3:
+            Search();
+            break;
+            case 4:
+            printf("프로그램을 종료합니다.\n");
+            list_pointer p = head;
+            while (p != NULL) {
+                list_pointer temp = p -> rlink;
+                free(p);
+                p = temp;
+            } // 프로그램 종료 전 할당받은 메모리 모두 반환.
+            return 0;
+            break;
+            default:
+            printf("올바른 메뉴를 선택하세요.\n");
+            break;
+        }
+    } while (iSelect != 4);
 }
