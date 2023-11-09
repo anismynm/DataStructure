6b75f6f352f10f61ba8da57395972755bdd00eda
anismynm <ddoe0318@naver.com>
2023-11-09 13:34:16 +0900

    d

--- 10주차/DoublyLinkedList.c
+++ 10주차/DoublyLinkedList.c
@@ -1,6 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 
 typedef struct list_node *list_pointer;
 typedef struct list_node {
